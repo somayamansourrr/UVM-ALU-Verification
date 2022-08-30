f4453c31f7a87d1fa26e36eb05c9c2df6a6e3809
somayamansourrr <111649086+somayamansourrr@users.noreply.github.com>
2022-08-30 13:21:15 +0400

    Add files via upload

--- add_if.sv
+++ add_if.sv
@@ -1,8 +1,9 @@
-interface add_if();
-  logic reset;
+interface add_if(input bit clk,reset);
+  //logic reset;
   logic [3:0] ALU_Sel;
   logic [7:0] A;
   logic [7:0] B;
   logic [7:0] ALU_Out;
   logic CarryOut;
+
 endinterface
